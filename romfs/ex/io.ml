023096f7833079a8294bf18a74ecd4184a949f06
Charles Averill <charlesaverill20@gmail.com>
2025-11-25 22:04:01 -0600

    Add screenshots

--- romfs/ex/io.ml
+++ romfs/ex/io.ml
@@ -1,16 +1,16 @@
 (* Input/Output *)
 
-print_string "Please enter your name: ";
+print_endline "Please enter your name: ";
 let name = read_line () in
 print_string "Hello, ";
 print_string name;
 print_endline "!";
-print_string "What's your favorite number? ";
+print_endline "What's your favorite number? ";
 let num = read_int () in
 print_string "I like ";
 print_int num;
 print_endline " too!";
-print_string "Do you know pi? ";
+print_endline "Do you know pi? ";
 let pi = read_float () in
 print_string "You said ";
 print_float pi;
